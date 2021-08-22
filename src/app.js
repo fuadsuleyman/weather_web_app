7451167aa493445bf69ac1b54aac5a582717d4fb
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-22 17:16:14 +0400

    customize views folder name and location

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ const chalk = require('../utils/chalk');
 const app = express();
 const port = 3000;
 
+// define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewspath = path.join(__dirname, '../templates');
 
+// setup handlebars engine and customize views location
 app.set('view engine', 'hbs');
+app.set('views', viewspath);
 
+// setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
